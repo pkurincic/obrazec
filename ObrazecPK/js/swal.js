ae72b76f0d1ee53ff48425911fea26ffe57750b5
Primož Kurinčič <77028970+pkurincic@users.noreply.github.com>
2022-03-16 07:59:29 +0100

    Add files via upload

---
+++ ObrazecPK/js/swal.js
@@ -0,0 +1,20 @@
+var toastMixin = Swal.mixin({
+    toast: true,
+    icon: 'success',
+    title: 'General Title',
+    animation: false,
+    position: 'top-right',
+    showConfirmButton: false,
+    timer: 3000,
+    timerProgressBar: true,
+    didOpen: (toast) => {
+      toast.addEventListener('mouseenter', Swal.stopTimer)
+      toast.addEventListener('mouseleave', Swal.resumeTimer)
+    }
+  });
+  document.querySelector(".btn").addEventListener('click', function(){
+  toastMixin.fire({
+    animation: true,
+    title: 'Signed in Successfully'
+  });
+});
